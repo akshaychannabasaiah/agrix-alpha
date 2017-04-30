58ea71a1285ef9c97418d51e9e8d977ba4478f10
Abhishek Shetty <abhishek10.rs@gmail.com>
2017-04-30 18:42:42 +0300

    test

--- client/imports/app/pestMap/pestMap.component.ts
+++ client/imports/app/pestMap/pestMap.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from "@angular/core";
+/*import {Component, OnInit} from "@angular/core";
 import { Observable } from "rxjs";
 import { PestMapDataService } from "./pestMap-data.service";
 import { PestLocation } from "../../../../both/models/demo.model";
 import template from "./demo.component.html";
-import style from "./demo.component.scss";
+import style from "./demo.component.scss";*/
