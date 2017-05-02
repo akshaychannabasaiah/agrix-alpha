7e5f9496403cf7b10ad1ee6ab6fde09c737957b3
AC <aksc.champ@gmail.com>
2017-05-03 02:05:45 +0300

    Added missing semicolon

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var fs = require("fs");
-fs.writeFile(path.join(__dirname, '..', 'keys/keys.ts'),"export var MapKey = '" + process.env.GMAP_KEY + "'",  function(err) {
+fs.writeFile(path.join(__dirname, '..', 'keys/keys.ts'),"export var MapKey = '" + process.env.GMAP_KEY + "';",  function(err) {
    if (err) {
       return console.error(err);
    }});
