d8504b9bec1568fa3935fa9ecf14cb4bba3f5467
Abhishek Shetty <abhishek10.rs@gmail.com>
2017-05-27 21:07:52 +0300

    styling changes to the forms

--- client/imports/app/components/topBar/top-bar.component.ts
+++ client/imports/app/components/topBar/top-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from "@angular/core";
 import {Location} from '@angular/common';
 import template from "./top-bar.component.html";
 import style from "./top-bar.component.scss";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "top-bar",
@@ -11,11 +12,15 @@ import style from "./top-bar.component.scss";
 export class TopBarComponent {
   @Input() title: string;
   tite : string;
-  constructor(private _location: Location) {
+  constructor(private _location: Location, public _router: Router) {
   }
 
   backpressed(){
-    
     this._location.back();
+  }
+
+  logout(){
+    Meteor.logout();
+    this._router.navigateByUrl('/');
   }
 }
