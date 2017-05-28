91862324d9bf6a4ffa2aab4795b29021c8bff9bf
AC <aksc.champ@gmail.com>
2017-05-28 20:00:24 +0300

    Added random area and Chnaged farmer model to contain pestID

--- both/models/field.model.ts
+++ both/models/field.model.ts
@@ -1,6 +1,7 @@
 import { Action } from "./action.model";
 import { LatLngLiteral } from 'angular2-google-maps/core';
 export interface Field {
+    pestID: Array<string>,
     name: string;
     area: number;
     cropType: string;
